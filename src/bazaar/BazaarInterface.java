030919135a65dcbe1e3f9ded7f239a99161cd101
rvinitra <rvinitra@gmail.com>
2016-03-04 15:29:44 -0500

    Indentation, Search through all neighbors in Bazaar

--- src/bazaar/BazaarInterface.java
+++ src/bazaar/BazaarInterface.java
@@ -4,7 +4,7 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface BazaarInterface extends Remote {
-	public void lookUp(LookupMsg lm) throws RemoteException;
-	public void reply(ReplyMsg seller) throws RemoteException;
+    public void lookUp(LookupMsg lm) throws RemoteException;
+    public void reply(ReplyMsg seller) throws RemoteException;
 
 }
--- src/bazaar/ReplyMsg.java
+++ src/bazaar/ReplyMsg.java
@@ -4,18 +4,13 @@ import java.io.Serializable;
 import java.util.Stack;
 
 public class ReplyMsg implements Serializable{
-    /**
-     * 
-     */
     private static final long serialVersionUID = 1L;
     Neighbor seller;
     public Stack<Neighbor> path;
-    public ReplyMsg(){
+    
+    public ReplyMsg(){}
 	
-    }
-	
-    public ReplyMsg(Neighbor seller,Stack<Neighbor> path)
-    {
+    public ReplyMsg(Neighbor seller,Stack<Neighbor> path){
 	this.seller=seller;
 	this.path=path;
     }
